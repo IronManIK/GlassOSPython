d1956610388884ac3072c9effa7860c8257a090f
Ishan <littleishan@gmail.com>
2021-12-06 13:34:43 -0500

    fix comments

--- PrototypeDisplay.py
+++ PrototypeDisplay.py
@@ -1,4 +1,3 @@
-#from tkinter.constants import FALSE
 from graphics import *
 import time
 import os
@@ -8,19 +7,10 @@ from pyowm.utils import config
 from pyowm.utils import timestamps
 from pyowm.weatherapi25.uris import DAILY_FORECAST_URI
 from WeatherAPI import *
+
 #\/----------------- UNCOMMENT THESE BEFORE RUNNING ON RPi ******************************************************************************
 #from gpiozero import CPUTemperature
 #cpu = CPUTemperature()
-
-#Open weather map setup
-#owm = OWM('5749f333920e0129251d1758df0a622a')
-#mgr = owm.weather_manager()
-#observation = mgr.weather_at_coords(42.291970, -83.535480) #setting it to my house
-#w = observation.weather
-
-#geting data
-#tempArray = w.temperature('fahrenheit')
-
 
 win = GraphWin("script", 240, 240, autoflush= False)
 win.setBackground(color_rgb(0, 0, 0))
